279d29d00039157d13f8877f2e80d5fa84b0ce36
cassiokamida <cassio.kida@hotmail.com>
2023-05-03 15:12:07 -0300

    att aula 1

---
+++ front-end/secao-2/dia-1-react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyHabilities from './about';
+
+class App extends React.Component {
+  render() {
+    return (<MyHabilities />);
+  }
+}
+
+export default App;
---
+++ front-end/secao-2/dia-1-react/src/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class MyHabilities extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Me chamo Cassio</h1>
+                <h2>Minhas Habilidades</h2>
+                <ul>
+                    <li>Comer</li>
+                    <li>Dormir</li>
+                    <li>Cagar</li>
+                    <li>Jogar</li>
+                    <li>Fazer absolutamente nada</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default MyHabilities
