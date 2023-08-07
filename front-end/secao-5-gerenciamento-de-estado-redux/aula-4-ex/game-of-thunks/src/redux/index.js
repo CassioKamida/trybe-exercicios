5d6bbe99aa31bae904397632704ce93b7d86fad9
cassiokamida <cassio.kida@hotmail.com>
2023-08-07 17:22:22 -0300

    aattt

---
+++ front-end/secao-5-gerenciamento-de-estado-redux/aula-4-ex/game-of-thunks/src/redux/index.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
+import characterReducer from './reducers/characterReducer';
+
